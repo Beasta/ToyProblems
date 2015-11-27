4da1ae836bb1690cc16fa0f169b6ae9b70d1819c
Barry Blaha <barryblaha@gmail.com>
2015-11-27 10:15:51 -0800

    Adding Function Cache

---
+++ functionCache/functionCache.js
@@ -0,0 +1,22 @@
+/*
+If you are calculating complex things or execute time-consuming API calls, you sometimes want to cache the results. In this case we want you to create a function wrapper, which takes a function and caches its results depending on the arguments, that were applied to the function.
+
+Usage example:
+
+var complexFunction = function(arg1, arg2) { /* complex calculation in here */ /*};
+var cachedFunction = cache(complexFunction);
+
+cachedFunction('foo', 'bar'); // complex function should be executed
+cachedFunction('foo', 'bar'); // complex function should not be invoked again, instead the cached result should be returned
+cachedFunction('foo', 'baz'); // should be executed, because the method wasn't invoked before with these arguments
+*/
+
+// Create your own tests here. These are some of the methods available:
+//  Test.expect(boolean, [optional] message) 
+//  Test.assertEquals(actual, expected, [optional] message)
+//  Test.assertSimilar(actual, expected, [optional] message)
+//  Test.assertNotEquals(actual, expected, [optional] message) 
+
+function cache(func) {
+  // do your magic here
+}

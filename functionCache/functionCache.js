a20f68bb85774402296e4e6b19799231f59b5581
Barry Blaha <barryblaha@gmail.com>
2015-11-27 16:06:59 -0800

    Working solution for functionCache

--- functionCache/functionCache.js
+++ functionCache/functionCache.js
@@ -1,5 +1,8 @@
+//from CodeWars
 /*
-If you are calculating complex things or execute time-consuming API calls, you sometimes want to cache the results. In this case we want you to create a function wrapper, which takes a function and caches its results depending on the arguments, that were applied to the function.
+If you are calculating complex things or execute time-consuming API calls, you sometimes want to cache the results.
+In this case we want you to create a function wrapper, which takes a function and caches its results depending on 
+the arguments, that were applied to the function.
 
 Usage example:
 
@@ -17,6 +20,51 @@ cachedFunction('foo', 'baz'); // should be executed, because the method wasn't i
 //  Test.assertSimilar(actual, expected, [optional] message)
 //  Test.assertNotEquals(actual, expected, [optional] message) 
 
+
 function cache(func) {
-  // do your magic here
+  var argResults = {}; //held in closure
+  //key will be the first argument, 
+  //value will be an object of objects, 
+  //where key is second arg and value is result
+  var theFunction = func; //held in closure
+  var depth = 0; //this will keep track of the current depth of the recursion
+  var curLocation = argResults;
+  var initialArgs;
+
+
+  var returnFunc = function (){
+
+    if(depth ===0 ){
+      initialArgs = arguments;
+    }
+
+    if(!curLocation[initialArgs[depth]]){//current location has not been visited
+      curLocation[initialArgs[depth]] = {}; //create a new storage for this combo of args
+      if (depth === initialArgs.length-1 /*|| initialArgs.length !==0*/){ // need to calculate and return results
+        var results = theFunction.apply(this,initialArgs); //calculate results
+        curLocation[initialArgs[depth]] = results; //save those results for later
+        depth = 0; //reset depth 
+        curLocation = argResults; //reset current location
+        return results;
+      }else if(initialArgs.length ===0){
+        return theFunction();
+      }else{
+        curLocation = curLocation[initialArgs[depth]];
+        depth++;
+        return returnFunc();
+      }      
+    }else{//this location has been visited
+      if (depth === initialArgs.length-1 ){
+        var results = curLocation[initialArgs[depth]];
+        depth =0;
+        curLocation = argResults;
+        return results; 
+      }else{
+        curLocation = curLocation[initialArgs[depth]];
+        depth++;
+        return returnFunc();
+      } 
+    } 
+  };
+  return returnFunc;
 }

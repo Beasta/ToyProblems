bb427d44e8d0dd82229375c559b76bc6d0eeac81
Barry Blaha <barryblaha@gmail.com>
2015-12-01 19:06:21 -0800

    Working solution for TrailingZerosOfN

--- NumberOfTrailingZerosOfN/NumberOfTrailingZerosOfN.js
+++ NumberOfTrailingZerosOfN/NumberOfTrailingZerosOfN.js
@@ -9,5 +9,16 @@
  that has 2 trailing zeros 4790016(00)
  Be careful 1000! has length of 2568 digital numbers.
  */
-
-
+/*
+  Test.assertEquals(zeros(12), 2)
+      Test.assertEquals(zeros(3), 0)
+*/
+function zeros (n) {
+ var i = 1;
+ var answer = 0;
+ while(n>=i){
+   i*=5;
+   answer += Math.floor(n/i);
+ }
+ return answer;
+}

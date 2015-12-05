945d902e3de4dddea26d8b22268f45b3de38c5b7
Barry Blaha <barryblaha@gmail.com>
2015-12-04 16:51:14 -0800

    Adding file for learning/playing Python

---
+++ Arrays-DS/pythonPlay.py
@@ -0,0 +1,33 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUTx = str(raw_input())
+#just playing with python
+"""
+x = str(raw_input())
+y = "awesome"
+#y = str(input())
+print x
+print y
+lineOne = x.splitlines()
+for i in xrange(4):
+        print lineOne
+ """
+"""
+N = int(input())
+
+#for line in range(N):
+    for line in range(N):
+          string = input()
+      print string 
+    """
+    
+t = input()
+#assert t<=10 and t>=1
+for _ in range(t):
+#s = list(raw_input())
+      s = raw_input().split(" ")
+      print s
+      #assert len(s) >=1 and len(s)<= 10000
+      #su = 0
+      #for i in range(0,len(s)/2):
+      #   su+= abs(ord(s[i]) - ord(s[-1-i]))
+      #print su
+

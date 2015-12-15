ef1a33823d0f48a4dbbb4726de6169d05e96cad0
Barry Blaha <barryblaha@gmail.com>
2015-12-15 14:58:38 -0800

    Workin my way through the ruby tutorial

--- rubyTutorial/ri20min.rb
+++ rubyTutorial/ri20min.rb
@@ -1,1 +1,12 @@
+## tutorial from https://www.ruby-lang.org/en/documentation/quickstart/
+#!/usr/bin/env ruby
 
+class MegaGreeter
+  attr_accessor :names
+
+  # Create the object
+  def initialize(name = "World")
+    @names = names
+  end
+
+  #say hi to everybody

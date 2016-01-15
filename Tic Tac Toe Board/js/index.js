49e71b4eceb1267f284cc6d9539d04a1d1885e6d
Barry Blaha <barryblaha@gmail.com>
2016-01-15 15:36:07 -0800

    Working logic to check for winner using function methods

--- Tic Tac Toe Board/js/index.js
+++ Tic Tac Toe Board/js/index.js
@@ -1,8 +1,7 @@
 var app = angular.module('ticToe',[]);
 app.controller('board',['$scope',function($scope){
-  
-
   $scope.xTurn = true;
+
   $scope.initialize = function(){
     $scope.board =  _.range(3).map(function(){
      return _.range(3).map(function(){
@@ -10,7 +9,9 @@ app.controller('board',['$scope',function($scope){
      });
     });
   };
+
   $scope.initialize();
+
   $scope.aClick = function(thisRow,thisColumn){
     if($scope.board[thisRow][thisColumn]===" "){
       var token = $scope.xTurn ? 'x' : 'o';
@@ -19,11 +20,24 @@ app.controller('board',['$scope',function($scope){
       console.log($scope.logic(token));
     }
   };
+  
   $scope.logic = function(token){ 
     return _.any($scope.board,function(row){//testing for winner on any row
       return _.every(row,function(column){
         return column === token;
       });
+    }) ||
+     _.any(_.zip.apply(_,$scope.board),function(row){// rotate the board and check for winner on any column
+      return _.every(row,function(column){
+        return column === token;
+      });
+    }) ||
+     $scope.board.every(function(row,rowIndex){ //check for a winner on diagonal from left to right
+        return row[rowIndex] === token;
+    }) ||
+      $scope.board.every(function(row,rowIndex){ //check for winner on diagonal from right to left
+        return row[$scope.board.length-1-rowIndex] === token;
     });
   };
+  
 }]);
